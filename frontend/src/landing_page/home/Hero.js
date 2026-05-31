e47a55fab45390c064d2790c73180410f1f7ccad
Kanak Maurya <kanakmaurya22@gmail.com>
2026-05-31 12:31:38 +0530

    testing with jest

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <div className='container p-5'>
       <div className='row text-center'>
-        <img src='media/images/homeHero.png' alt='' className='mb-5' />
+        <img src='media/images/homeHero.png' alt='Hero Image' className='mb-5' />
         <h1 className='mt-5 '>Invest in everything</h1>
         <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.
 
